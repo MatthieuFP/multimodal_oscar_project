94736c5108a92008a122b8e92e137b40e11dea4c
mfuteral <mfuteral@wifi-pro-82-179.paris.inria.fr>
2023-02-23 12:06:11 +0100

    update multiprocessing pipeline

--- parse_warc.py
+++ parse_warc.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from fastwarc.warc import ArchiveIterator, is_http
 from fastwarc.stream_io import *
 from resiliparse.parse.html import HTMLTree, traverse_dom, DOMNode, DOMContext
+from multiprocessing import Pool, cpu_count
 
 file = "CC-MAIN-20220924151538-20220924181538-00000.warc.gz"
 stream = GZipStream(open(file, 'rb'))
@@ -15,27 +16,36 @@ class SaveDocument:
         self.images = []
         self.url = {}
         self.lang = None
+        self.alt_detected = False
+
     def add_url(self, url):
         self.url[url] = 1
+
     def check_url(self, url):
         return True if url in self.url.keys() else False
+    
+    def is_valid(self):
+        if not self.images:
+            return False
+        elif self.images and not self.alt_detected and not self.document:
+            return False
+        else:
+            return True
 
 
-def save_node(node_ctx: DOMContext):
-    global img_tag, txt_tag
+def save_node(node_ctx: DOMContext, Document: SaveDocument):
     node = node_ctx.node
     depth = node_ctx.depth
     if node.tag == "img":
         if "src" in node.attrs and node["src"].startswith("http") and node["src"].endswith(
                 ("jpg", "jpeg", "png")):
-            img_tag = True
             sample = {"url": node["src"], "depth": depth}
             if not Document.check_url(node["src"]):
                 Document.add_url(node["src"])
                 if "alt" in node.attrs:
                     _alt = node["alt"].strip().strip('\n')
                     if _alt:
-                        txt_tag = True
+                        Document.alt_detected = True
                         sample["alt"] = _alt
                 Document.images.append(sample)
     else:
@@ -44,22 +54,35 @@ def save_node(node_ctx: DOMContext):
                 text = node.text.strip().strip("\n")
                 text = text.replace("\t", "").replace("\n", "").strip()
                 if text:
-                    txt_tag = True
                     Document.document.append({"tag": node.tag, "depth": depth, "text": text})
 
 
-out = {}
-for record in tqdm(ArchiveIterator(stream, func_filter=is_http)):
-    img_tag, txt_tag = False, False
+def process_html(record):
     if record.content_length > 1000:  # Bytes limit
         body = record.reader.read()
         tree = HTMLTree.parse_from_bytes(body)
-        imgs = []
         Document = SaveDocument()
         # Extract lang
         if "lang" in tree.document.get_elements_by_tag_name("html")[0].attrs:
             Document.lang = tree.document.get_elements_by_tag_name("html")[0]["lang"]
-        traverse_dom(base_node=tree.document, start_callback=save_node, elements_only=False)
+        traverse_dom(base_node=tree.document, start_callback=lambda node: save_node(node, Document), elements_only=False)
         title = tree.title.strip().strip("\n")
-        if img_tag and txt_tag:
-            out[record.headers['WARC-Record-ID']] = {"title": title, "document": Document}
+        if Document.is_valid():
+            return {record.headers['WARC-Record-ID']: {"title": title, "document": Document}}
+        else:
+            return None
+    else:
+        return None
+
+
+def main():
+    pool = Pool(cpu_count() // 2)
+    warc_iter = ArchiveIterator(stream, func_filter=is_http)
+    iterator = pool.imap_unordered(process_html, warc_iter)
+    iterator = tqdm(iterator, desc='Processing HTML')
+    out = {k: v for sample in iterator if sample is not None for k, v in sample.items()}
+    return out
+
+
+if __name__ == "__main__":
+    out = main()
